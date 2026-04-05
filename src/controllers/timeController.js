649800ea3617e849d19d9e70934f744ddae7d10e
mirkojr <footsbest@gmail.com>
2026-04-05 15:00:36 -0300

    adicionando rotas e logica para times

--- src/controllers/timeController.js
+++ src/controllers/timeController.js
@@ -8,5 +8,56 @@ export default {
         } catch (error) {
             res.status(400).json({message: "Busca de usuário falhou", error})
         }
+    },
+
+    async show(req, res){
+        try {
+            const time = await Time.findByPk(req.params.id);
+            if(!time) return res.status(404).json({message: "Time não encontrado"});
+            res.status(200).json(time);
+        } catch (error) {
+            res.status(400).json({message: "Busca de usuário falhou", error})
+        }
+    },
+
+    async searchByName(req, res){
+        try {
+            const times = await Time.findAll({
+                where: {
+                    nome: req.params.nome
+                }
+            });
+            res.status(200).json(times);
+        } catch (error) {
+            res.status(400).json({message: "Busca de usuário falhou", error})
+        }
+    },
+
+    async store(req, res){
+        try {
+            const novoTime = await Time.create(req.body);
+            res.status(201).json(novoTime);
+        } catch (error) {
+            res.status(400).json({message: "Criação de usuário falhou", error})
+        }
+    },
+
+    async update(req, res){
+        try {
+            await Time.update(req.body, {where: {id: req.params.id}});
+            res.status(200).json({message: "Time atualizado com sucesso"});
+        } catch (error) {
+            res.status(400).json({message: "Atualização de usuário falhou", error})
+        }
+    },
+
+    async delete(req, res){
+        try {
+            await Time.destroy({where: {id: req.params.id}});
+            res.status(204).send();
+        } catch (error) {
+            res.status(400).json({message: "Deleção de usuário falhou", error})
+        }
     }
+
 }
--- src/routes/timeRoutes.js
+++ src/routes/timeRoutes.js
@@ -1,8 +1,14 @@
 import express from 'express'
 import timeController from '../controllers/timeController.js';
+import { authMiddleware, adminOnly } from '../middlewares/authMiddleware.js';
 
 const router = express.Router();
 
-router.get('/', timeController.index);
+router.get('/', authMiddleware, timeController.index);
+router.get('/:id', authMiddleware, timeController.show);
+router.get('/busca/:nome', authMiddleware, timeController.searchByName);
+router.post('/', authMiddleware, adminOnly, timeController.store);
+router.put('/:id', authMiddleware, adminOnly, timeController.update);
+router.delete('/:id', authMiddleware, adminOnly, timeController.delete);
 
 export default router;
